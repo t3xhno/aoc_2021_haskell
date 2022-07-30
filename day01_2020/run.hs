b307ad18e39a59948cddf037a9961b09924f8e43
t3xhno <texhnolainx@gmail.com>
2022-07-30 15:59:17 +0200

    haskell | Refactor names

--- day01_2020/run.hs
+++ day01_2020/run.hs
@@ -1,11 +1,11 @@
 import Lib.IO (readLines)
 
-solve :: (Eq a, Num a) => [a] -> a
-solve xs = head [x * y | x <- xs, y <- xs, x + y == 2020]
+solve_1 :: (Eq a, Num a) => [a] -> a
+solve_1 xs = head [x * y | x <- xs, y <- xs, x + y == 2020]
 
-solve' :: (Eq a, Num a) => [a] -> a
-solve' xs = head [x * y * z | x <- xs, y <- xs, z <- xs, x + y + z == 2020]
+solve_2 :: (Eq a, Num a) => [a] -> a
+solve_2 xs = head [x * y * z | x <- xs, y <- xs, z <- xs, x + y + z == 2020]
 
 main = do
     contents <- readLines
-    print $ map ($ map read contents) [solve, solve']
+    print $ map ($ map read contents) [solve_1, solve_2]
